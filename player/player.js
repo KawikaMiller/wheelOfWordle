3a9660124e0fbed776048c329ab3a60ed899ccda
KMART <93807261+KMArtwork@users.noreply.github.com>
2023-04-20 17:37:03 -0700

    turns taking & game over works

--- player/player.js
+++ player/player.js
@@ -5,19 +5,22 @@ const prompt = require('prompt-sync')();
 const { io } = require('socket.io-client');
 const SERVER_URL = process.env.SERVER_URL
 const eventPool = require('../eventPool')
+const Chance = require('chance')
+const chance = new Chance();
 
 const socket = io(SERVER_URL)
 
 let player = {
-  name: 'testPlayer',
+  name: chance.name(),
   id: 1,
   score: 0,
 }
 
+socket.on(eventPool[5], (payload) => {
+  console.log(payload.revealedWord)
 
-socket.on(eventPool[5], (turnId) => {
-  console.log('IDs', turnId, player.id)
-  if(turnId === player.id){
+  if(payload.turnId === player.id){
+    console.log('YOUR TURN! \n')
     const guessLetter = prompt('Guess a letter: ');
     socket.emit(eventPool[3], guessLetter)
   }
@@ -26,18 +29,15 @@ socket.on(eventPool[5], (turnId) => {
 socket.on(eventPool[4], (score) =>{
   player.score += score;
   console.log('Score: ', player.score)
+  console.log('Waiting for other player\'s turn...')
 })
 
-socket.on(eventPool[2], (player) =>{
-  player = player; 
-  console.log('PLAYER ID', player);
+socket.on(eventPool[2], (updatedPlayer) =>{
+  player = updatedPlayer; 
 })
 
-socket.on(eventPool[1], (turnId) =>{
-  if(turnId === player.id){
-    const guessLetter = prompt('Guess a letter: ');
-    socket.emit(eventPool[3], guessLetter)
-  }
+socket.on(eventPool[1], (message) =>{
+  console.log(message)
 })
 
 socket.on(eventPool[0], (player) => {
--- server.js
+++ server.js
@@ -17,41 +17,74 @@ io.on('connection', (socket) => {
   console.log('CLIENT CONNECTED TO SERVER: ', socket.id)
 
   socket.on(eventPool[0], (player) => {
-    player.id = playerQueue.players.length + 1;
-    playerQueue.addPlayer(player);
+    let updatedPlayer = playerQueue.addPlayer(player);
 
-    io.to(socket.id).emit(eventPool[2], player)
-
-    // console.log(usersArray);
+    io.to(socket.id).emit(eventPool[2], updatedPlayer)
 
     socket.join('gameRoom');
     console.log(`${player.name} HAS JOINED THE GAME ROOM`)
 
     // socket.to('gameRoom').emit(eventPool[0], player)
     socket.emit(eventPool[0], player)
+
     const clientsInRoom = socket.adapter.rooms.get('gameRoom');
     if (clientsInRoom.size >= 2) {
-      socket.emit(eventPool[1], (turnId))
+
+      let payload = {
+        turnId: turnId,
+        revealedWord: revealedWord,
+      }
+
+      socket.to('gameRoom').emit(eventPool[5], payload)
   }
 })
-
-  socket.on(eventPool[1], (player) => {
-
-  })
 
   socket.on(eventPool[3], (guessLetter) =>{
     secretWord.forEach((letter, idx) => {
       if(guessLetter === letter){
         revealedWord[idx] = secretWord[idx]
+        playerQueue.players[turnId - 1].score++;
         socket.emit(eventPool[4], 1)
       }
     })
-    console.log('REVEALED WORD', revealedWord);
+
+    secretWord.every((letter) => {
+      if (letter !== guessLetter) {
+        socket.emit(eventPool[1], 'Better luck next time! \n Waiting for other player\'s turn...')
+      }
+    })
+
+    let isGameOver = revealedWord.every((letter) => {
+      if (letter !== 0) {
+        return true;
+      }
+    })
+
+    if (isGameOver) {
+      let highscore = 0;
+      let winner;
+      playerQueue.players.forEach(player => {
+        if (player.score > highscore){
+          winner = player;
+        }
+      })
+      socket.emit(eventPool[1], `GAME OVER!! Winner is ${winner.name} with ${winner.score} points!`);
+
+      socket.to(`gameRoom`).emit(eventPool[1], `GAME OVER!! Winner is ${winner.name} with ${winner.score} points!`);
+      return;
+    }
+
     turnId ++;
     if(turnId > playerQueue.players.length){
       turnId = 1;
     }
-    socket.emit(eventPool[5], turnId);
+
+    let payload = {
+      turnId: turnId,
+      revealedWord: revealedWord,
+    }
+
+    socket.to('gameRoom').emit(eventPool[5], payload);
   })
 
 
